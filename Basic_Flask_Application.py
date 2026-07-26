9418408c67ec73f28e4220b2684877bdc85b0d58
Rahul SD <ra.devops.world@gmail.com>
2026-07-27 00:55:41 +0530

    Created Basic Flask Application Python File

--- Basic_Flask_Application.py
+++ Basic_Flask_Application.py
@@ -1,1 +1,6 @@
+# What you need to build: A Python fi le that runs a local web server with two working endpoints.
+# Endpoint Expected Response / Welcome to app
+# Endpoint Expected Response /health App is running 
+# How to verify your work: Visit http://localhost:5000/ and http://localhost:5000/health in your browser. Both should show the correct text.
+# Ensure: ● The application runs successfully on localhost , ● All endpoints return the correct response and are accessible via browser or Postman
 
