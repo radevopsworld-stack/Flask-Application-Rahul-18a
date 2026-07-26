0744a9d4a29f8d2f7d79e9e2fef5373adbb7a601
Rahul SD <ra.devops.world@gmail.com>
2026-07-27 01:22:19 +0530

    Added the Health App is running route to Dev

--- Basic_Flask_Application.py
+++ Basic_Flask_Application.py
@@ -15,7 +15,7 @@ def home():
 
 @app.route("/health")
 def health():
-    return "health App is running!" 
+    return "Health App is running!" 
 if __name__ == "__main__":
     app.run(debug=True)
     
